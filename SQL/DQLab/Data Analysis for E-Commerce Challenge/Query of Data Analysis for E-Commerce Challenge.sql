4bc250074854010f65e48caf37cc744f30e53f29
Firza Chandra Sandjaya Putra <102524044+FirzaCank@users.noreply.github.com>
2022-04-23 20:30:42 +0700

    Update Query of Data Analysis for E-Commerce Challenge.sql

--- SQL/DQLab/Data Analysis for E-Commerce Challenge/Query of Data Analysis for E-Commerce Challenge.sql
+++ SQL/DQLab/Data Analysis for E-Commerce Challenge/Query of Data Analysis for E-Commerce Challenge.sql
@@ -1,1 +1,48 @@
+-- 10 Transaksi terbesar user 12476
 
+SELECT
+  seller_id,
+  buyer_id, 
+  total as nilai_transaksi,
+  created_at as tanggal_transaksi
+FROM 
+  orders
+WHERE 
+  buyer_id = 12476
+GROUP BY 
+  order_id
+ORDER BY 
+  3 desc
+limit 10;
+
+
+-- Transaksi per bulan tahun 2020
+
+SELECT 
+	extract(YEAR_MONTH FROM created_at) as tahun_bulan,
+  COUNT(1) jumlah_transaksi, 
+  SUM(total) total_transaksi
+FROM 
+	orders
+WHERE 
+	created_at >= '2020-01-01'
+GROUP BY 
+	tahun_bulan
+ORDER BY 
+	tahun_bulan;
+  
+  
+  -- Pengguna dengan rata-rata transaksi terbesar di Januari 2020
+
+SELECT
+	buyer_id,
+    count(1) jumlah_transaksi,
+    AVG(total) rata_rata_transaksi
+FROM orders
+WHERE created_at BETWEEN '2020-01-01' AND '2020-02-01'
+GROUP BY 
+	buyer_id
+HAVING COUNT(1) >= 2
+ORDER BY 
+	rata_rata_transaksi DESC
+limit 10;
