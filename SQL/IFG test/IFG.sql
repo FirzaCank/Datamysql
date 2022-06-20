a8a79886652f820955c49ca5f485c3de8280764e
Firza Chandra Sandjaya Putra <102524044+FirzaCank@users.noreply.github.com>
2022-06-20 20:14:24 +0700

    Create IFG.sql

---
+++ SQL/IFG test/IFG.sql
@@ -0,0 +1,18 @@
+CREATE TABLE buggy (
+    id INT,
+    name VARCHAR(10) CHARACTER SET utf8,
+    bug INT CHARACTER SET utf8,
+);
+
+INSERT INTO buggy
+VALUES
+	(1, 'andi', 0),
+    (2, 'andi', 1),
+    (3, 'reza', 0),
+    (4, 'reza', 1),
+    (5, 'dinda', 1),
+    (6, 'malik', 1),
+    (7, 'kosas', 0),
+    (8, 'malik', 1),
+    (9, 'eja', 1),
+    (10, 'kosas', 0);
