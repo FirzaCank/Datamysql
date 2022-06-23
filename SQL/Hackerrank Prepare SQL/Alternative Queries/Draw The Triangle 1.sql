2892476c6bf540f3426d276e5e1236845334364c
Firza Chandra Sandjaya Putra <102524044+FirzaCank@users.noreply.github.com>
2022-06-23 18:18:22 +0700

    Create Draw The Triangle 1.sql

---
+++ SQL/Hackerrank Prepare SQL/Alternative Queries/Draw The Triangle 1.sql
@@ -0,0 +1,36 @@
+/*P(R) represents a pattern drawn by Julia in R rows.
+The following pattern represents P(5):
+*/
+
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+
+*/Write a query to print the pattern P(20)! /*
+
+1. OPTION 1 (MySQL) :
+
+SELECT
+    REPEAT('* ', @NUMBER := @NUMBER - 1)
+FROM information_schema.tables, (SELECT @NUMBER:=21) t LIMIT 20;
+
+
+
+2. OPTION 2 (MySQL) :
+
+SET @NUMBER = 21;
+SELECT
+    REPEAT("* ", @NUMBER := @NUMBER -1)
+FROM information_schema.tables;
+
+
+3. OPTION 3 (MS SQL SERVER) :
+
+DECLARE @i INT = 20
+WHILE (@i > 0)
+BEGIN
+    PRINT REPLICATE('* ', @i)
+    SET @i = @i -1
+END;
