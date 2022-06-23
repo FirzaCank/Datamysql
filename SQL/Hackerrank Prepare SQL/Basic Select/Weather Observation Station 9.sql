55c24b01c8670e6b32e8ea1a7f5c653b175f3f04
Firza Chandra Sandjaya Putra <102524044+FirzaCank@users.noreply.github.com>
2022-06-23 17:28:39 +0700

    Create Weather Observation Station 9.sql

---
+++ SQL/Hackerrank Prepare SQL/Basic Select/Weather Observation Station 9.sql
@@ -0,0 +1,8 @@
+/*Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates./*
+
+
+SELECT
+    DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) NOT IN ('A','I','U','E','O')
+ORDER BY CITY;
